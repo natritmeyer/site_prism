06762f4a845b3b6c193f4c77b7d0260d9db99136
Nat Ritmeyer <nat@natontesting.com>
2011-12-13 16:24:48 +0000

    adding documentation to Page

--- lib/prismatic/page.rb
+++ lib/prismatic/page.rb
@@ -1,24 +1,29 @@
 module Prismatic
   class Page
-    #visits the url associated with this page
+    # Visits the url associated with this page
+    # @raise [Prismatic::NoUrlForPage] To load a page the url must be set using Page#load
     def load
       raise Prismatic::NoUrlForPage if url.nil?
     end
     
-    #set the url associated with this page
-    #  class SearchPage < Prismatic::Page
-    #    set_url "/search"
-    #  end
+    # Set the url associated with this page
+    # @param [String] URL the portion of the url that identifies this page when appended onto Capybara's app_host. Calling {Prismatic::Page#load} causes Capybara to visit this page.
+    # @example Setting a page's url
+    #   class SearchPage < Prismatic::Page
+    #     set_url "/search"
+    #   end
     def self.set_url input
       @url = input
     end
     
-    #get the url associated with this page
+    # Get the url associated with this page
+    # @see Prismatic::Page#url
     def self.url
       @url
     end
     
-    #get the url associated with this page
+    # Get the url associated with this page
+    # @see Prismatic::Page.url
     def url
       self.class.url
     end
