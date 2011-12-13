0db8fb785ebfd9def63104463782d3866f9f9673
Nat Ritmeyer <nat@natontesting.com>
2011-12-13 11:10:35 +0000

    adding ability to set the url for a page

--- lib/prismatic/page.rb
+++ lib/prismatic/page.rb
@@ -1,7 +1,17 @@
 module Prismatic
   class Page
+    @@url = ""
+    
     def load
       
+    end
+    
+    def self.set_url input
+      @@url = input
+    end
+    
+    def url
+      @@url
     end
   end
 end
--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -4,4 +4,16 @@ describe Prismatic::Page do
   it "should respond to load" do
     Prismatic::Page.new.should respond_to :load
   end
+  
+  it "should respond to set_url" do
+    Prismatic::Page.should respond_to :set_url
+  end
+  
+  it "should be able to set a url against it" do
+    class MyPage < Prismatic::Page
+      set_url "bob"
+    end
+    my_page = MyPage.new
+    my_page.url.should == "bob"
+  end
 end
