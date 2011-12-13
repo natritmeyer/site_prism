d0e7ef781f1cfde1e8ee8474d3b7c050daf6ce32
Nat Ritmeyer <nat@natontesting.com>
2011-12-13 14:19:01 +0000

    adding doco to the page class

--- lib/prismatic/page.rb
+++ lib/prismatic/page.rb
@@ -1,17 +1,24 @@
 module Prismatic
   class Page
+    #visits the url associated with this page
     def load
       raise Prismatic::NoUrlForPage if url.nil?
     end
     
+    #set the url associated with this page
+    #  class SearchPage < Prismatic::Page
+    #    set_url "/search"
+    #  end
     def self.set_url input
       @url = input
     end
     
+    #get the url associated with this page
     def self.url
       @url
     end
     
+    #get the url associated with this page
     def url
       self.class.url
     end
