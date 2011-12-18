4357fd26baa612c69703fdbe11bf00aa1b003ae8
Nat Ritmeyer <nat@natontesting.com>
2011-12-18 20:53:15 +0000

    adding exception doco

--- lib/prismatic/exceptions.rb
+++ lib/prismatic/exceptions.rb
@@ -1,4 +1,6 @@
 module Prismatic
+  # Raised if you ask a page to load but it hasn't had its url set
   class NoUrlForPage < StandardError; end
+  # Raised if you check to see if a page is displayed but it hasn't had its url matcher set
   class NoUrlMatcherForPage < StandardError; end
 end
