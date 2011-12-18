3252329a28de82ed7f0cb3ca8c886d7df37a60db
Nat Ritmeyer <nat@natontesting.com>
2011-12-18 20:50:31 +0000

    updating page doco

--- lib/prismatic/page.rb
+++ lib/prismatic/page.rb
@@ -3,31 +3,46 @@ module Prismatic
     include Capybara::DSL
     
     # Visits the url associated with this page
-    # @raise [Prismatic::NoUrlForPage] To load a page the url must be set using {#load}
+    # @raise [Prismatic::NoUrlForPage] To load a page the url must be set using {.set_url}
     def load
       raise Prismatic::NoUrlForPage if url.nil?
       visit url
     end
     
     # Checks to see if we're on this page or not
-    # @return true if the current url matches the url matcher that has been set, false if it doesn't
+    # @return true if the browser's current url matches the {.url_matcher} that has been set, false if it doesn't
+    # @raise [Prismatic::NoUrlMatcherForPage] To check whether we're on this page or not the url matcher must be set using {.set_url_matcher}
+    # @example
+    #   class SearchPage < Prismatic::Page
+    #     set_url_matcher /\/search.htm$/
+    #   end
+    #   search_page = SearchPage.new
+    #   search_page.load
+    #   puts "We're on the search page" if search_page.displayed?
+    #   search_page.should be_displayed
     def displayed?
       raise Prismatic::NoUrlMatcherForPage if url_matcher.nil?
       !(page.current_url =~ url_matcher).nil?
     end
     
     # Set the url associated with this page
-    # @param [String] URL the portion of the url that identifies this page when appended onto Capybara's app_host. Calling {Prismatic::Page#load} causes Capybara to visit this page.
-    # @example Setting a page's url
+    # @param [String] page_url the portion of the url that identifies this page when appended onto Capybara's app_host. Calling {Prismatic::Page#load} causes Capybara to visit this page.
+    # @example
     #   class SearchPage < Prismatic::Page
-    #     set_url "/search"
+    #     set_url "/search.htm"
     #   end
-    def self.set_url input
-      @url = input
+    def self.set_url page_url
+      @url = page_url
     end
     
-    def self.set_url_matcher input
-      @url_matcher = input
+    # Set the url matcher associated with this page
+    # @param [Regexp] page_url_matcher a regular expression that when compared to the current browser url will match if we're on this page or not match if we're not on this page
+    # @example
+    #   class SearchPage < Prismatic::Page
+    #     set_url_matcher /\/search.htm$/
+    #   end
+    def self.set_url_matcher page_url_matcher
+      @url_matcher = page_url_matcher
     end
     
     # Get the url associated with this page
@@ -37,6 +52,9 @@ module Prismatic
       @url
     end
     
+    # Get the url matcher associated with this page
+    # @see Prismatic::Page#url_matcher
+    # @return [Regexp] the url matcher originally set in {.set_url_matcher}
     def self.url_matcher
       @url_matcher
     end
@@ -47,6 +65,8 @@ module Prismatic
       self.class.url
     end
     
+    # Get the url matcher associated with this page
+    # @see Prismatic::Page.url_matcher
     def url_matcher
       self.class.url_matcher
     end
