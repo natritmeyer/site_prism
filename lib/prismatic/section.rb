4570843d16daeeee3c15df0b64fb7dcdbf0b4bea
Nat Ritmeyer <nat@natontesting.com>
2011-12-21 21:36:55 +0000

    removing old attribute that was never used

--- lib/prismatic/section.rb
+++ lib/prismatic/section.rb
@@ -2,18 +2,18 @@ module Prismatic
   class Section
     extend ElementContainer
     
-    attr_reader :root_element_locator
-  
     def initialize root_element
       @root_element = root_element
     end
     
     private
     
+    # Section specific element finder
     def find_one locator
       @root_element.find locator
     end
     
+    # Section specific elements finder
     def find_all locator
       @root_element.all locator
     end
