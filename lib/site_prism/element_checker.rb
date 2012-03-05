d0b8c019d16b1b36f58fa2ffee9968091206213f
Nat Ritmeyer <nat@natontesting.com>
2012-03-05 13:13:08 +0000

    preventing all_there? from waiting for elements

--- lib/site_prism/element_checker.rb
+++ lib/site_prism/element_checker.rb
@@ -1,6 +1,8 @@
 module SitePrism::ElementChecker
   def all_there?
-    !self.class.element_names.map {|element| self.send "has_#{element}?" }.include? false
+    Capybara.using_wait_time(0) do
+      !self.class.element_names.map {|element| self.send "has_#{element}?" }.include? false
+    end
   end
 end
 
