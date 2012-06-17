923c724e2845530c176bd1d0ccbcd7c36f027cef
Nat Ritmeyer <nat@natontesting.com>
2012-06-17 02:41:01 +0100

    refactoring all_there? based on https://github.com/remi/site_prism/commit/908e93e1963fc7cfff12e637582bedb997da3069

--- lib/site_prism/element_checker.rb
+++ lib/site_prism/element_checker.rb
@@ -1,7 +1,7 @@
 module SitePrism::ElementChecker
   def all_there?
     Capybara.using_wait_time(0) do
-      !self.class.element_names.map {|element| self.send "has_#{element}?" }.include? false
+      self.class.element_names.all? {|element| send "has_#{element}?" }
     end
   end
 end
