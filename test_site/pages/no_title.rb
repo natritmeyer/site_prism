e3f10964b948d98aae665bed7e55d7d052f32be8
Nat Ritmeyer <nat@natontesting.com>
2012-11-21 23:59:22 +0000

    capybara no longer seems to like urls that start with file://

--- test_site/pages/no_title.rb
+++ test_site/pages/no_title.rb
@@ -1,5 +1,5 @@
 class TestNoTitle < SitePrism::Page
-  set_url "/no_title.htm"
+  set_url "file://" + Dir.pwd + "/test_site/html/no_title.htm"
 
   element :element_without_selector
   elements :elements_without_selector
--- test_site/pages/section_experiments.rb
+++ test_site/pages/section_experiments.rb
@@ -1,5 +1,5 @@
 class TestSectionExperiments < SitePrism::Page
-  set_url '/section_experiments.htm'
+  set_url "file://" + Dir.pwd + "/test_site/html/section_experiments.htm"
 
   section :parent_section, Parent, '.parent-div'
 
