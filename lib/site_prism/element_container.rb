b3c1c79623a0f508c8d23dfd4490327981a4eb45
Nat Ritmeyer <nat@natontesting.com>
2013-04-03 14:54:41 +0100

    change method names, changed how create_no_selector works

--- lib/site_prism/element_container.rb
+++ lib/site_prism/element_container.rb
@@ -1,6 +1,6 @@
 module SitePrism::ElementContainer
 
-  def element element_name, *find_args
+  def element(element_name, *find_args)
     build element_name, *find_args do
       define_method element_name.to_s do
         find_first *find_args
@@ -8,7 +8,7 @@ module SitePrism::ElementContainer
     end
   end
 
-  def elements collection_name, *find_args
+  def elements(collection_name, *find_args)
     build collection_name, *find_args do
       define_method collection_name.to_s do
         find_all *find_args
@@ -17,7 +17,7 @@ module SitePrism::ElementContainer
   end
   alias :collection :elements
 
-  def section section_name, section_class, *find_args
+  def section(section_name, section_class, *find_args)
     build section_name, *find_args do
       define_method section_name do
         section_class.new self, find_first(*find_args)
@@ -25,7 +25,7 @@ module SitePrism::ElementContainer
     end
   end
 
-  def sections section_collection_name, section_class, *find_args
+  def sections(section_collection_name, section_class, *find_args)
     build section_collection_name, *find_args do
       define_method section_collection_name do
         find_all(*find_args).collect do |element|
@@ -35,7 +35,7 @@ module SitePrism::ElementContainer
     end
   end
 
-  def iframe iframe_name, iframe_page_class, iframe_id
+  def iframe(iframe_name, iframe_page_class, iframe_id)
     add_to_mapped_items iframe_name
     create_existence_checker iframe_name, iframe_id
     create_waiter iframe_name, iframe_id
@@ -46,7 +46,7 @@ module SitePrism::ElementContainer
     end
   end
 
-  def add_to_mapped_items item
+  def add_to_mapped_items(item)
     @mapped_items ||= []
     @mapped_items << item.to_s
   end
@@ -57,34 +57,34 @@ module SitePrism::ElementContainer
 
   private
 
-  def build name, *find_args
+  def build(name, *find_args)
     if find_args.empty?
       create_no_selector name
     else
       add_to_mapped_items name
       yield
     end
-    add_checkers_and_waiters name, *find_args
+    add_helper_methods name, *find_args
   end
 
-  def add_checkers_and_waiters name, *find_args
+  def add_helper_methods(name, *find_args)
     create_existence_checker name, *find_args
     create_waiter name, *find_args
     create_visibility_waiter name, *find_args
     create_invisibility_waiter name, *find_args
   end
 
-  def build_checker_or_waiter element_name, proposed_method_name, *find_args
+  def create_helper_method(proposed_method_name, *find_args)
     if find_args.empty?
-      create_no_selector element_name, proposed_method_name
+      create_no_selector proposed_method_name
     else
       yield
     end
   end
 
-  def create_existence_checker element_name, *find_args
+  def create_existence_checker(element_name, *find_args)
     method_name = "has_#{element_name.to_s}?"
-    build_checker_or_waiter element_name, method_name, *find_args do
+    create_helper_method method_name, *find_args do
       define_method method_name do
         Capybara.using_wait_time 0 do
           element_exists? *find_args
@@ -93,9 +93,9 @@ module SitePrism::ElementContainer
     end
   end
 
-  def create_waiter element_name, *find_args
+  def create_waiter(element_name, *find_args)
     method_name = "wait_for_#{element_name.to_s}"
-    build_checker_or_waiter element_name, method_name, *find_args do
+    create_helper_method method_name, *find_args do
       define_method method_name do |timeout = Capybara.default_wait_time|
         Capybara.using_wait_time timeout do
           element_exists? *find_args
@@ -104,9 +104,9 @@ module SitePrism::ElementContainer
     end
   end
 
-  def create_visibility_waiter element_name, *find_args
+  def create_visibility_waiter(element_name, *find_args)
     method_name = "wait_until_#{element_name.to_s}_visible"
-    build_checker_or_waiter element_name, method_name, *find_args do
+    create_helper_method method_name, *find_args do
       define_method method_name do |timeout = Capybara.default_wait_time|
         Capybara.using_wait_time timeout do
           element_exists? *find_args
@@ -118,9 +118,9 @@ module SitePrism::ElementContainer
     end
   end
 
-  def create_invisibility_waiter element_name, *find_args
+  def create_invisibility_waiter(element_name, *find_args)
     method_name = "wait_until_#{element_name.to_s}_invisible"
-    build_checker_or_waiter element_name, method_name, *find_args do
+    create_helper_method method_name, *find_args do
       define_method method_name do |timeout = Capybara.default_wait_time|
         Timeout.timeout timeout, SitePrism::TimeOutWaitingForElementInvisibility do
           Capybara.using_wait_time 0 do
@@ -131,10 +131,9 @@ module SitePrism::ElementContainer
     end
   end
 
-  def create_no_selector element_name, method_name = nil
-    no_selector_method_name = method_name.nil? ? element_name : method_name
-    define_method no_selector_method_name do
-      raise SitePrism::NoSelectorForElement.new("#{self.class.name} => :#{element_name} needs a selector")
+  def create_no_selector(method_name)
+    define_method method_name do
+      raise SitePrism::NoSelectorForElement.new("#{self.class.name} => :#{method_name} needs a selector")
     end
   end
 end
