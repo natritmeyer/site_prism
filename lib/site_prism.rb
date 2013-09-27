1fc8ade9d7daf198e4025129ca6f0b129e336a2a
Paul Yakshankin <yakspavel@undev.ru>
2013-09-27 19:25:16 +0000

    replaced eager_load by lazy_load

--- lib/site_prism.rb
+++ lib/site_prism.rb
@@ -1,8 +1,17 @@
-require 'addressable/template'
-require 'site_prism/version'
-require 'site_prism/exceptions'
-require 'site_prism/element_container'
-require 'site_prism/element_checker'
-require 'site_prism/page'
-require 'site_prism/section'
+module SitePrism
+  autoload :Addressable,  'addressable/template'
+  autoload :Version,  'site_prism/version'
+  autoload :ElementContainer,  'site_prism/element_container'
+  autoload :ElementChecker,  'site_prism/element_checker'
+  autoload :Page,  'site_prism/page'
+  autoload :Section,  'site_prism/section'
 
+  autoload :NoUrlForPage,  'site_prism/exceptions'
+  autoload :NoUrlMatcherForPage,  'site_prism/exceptions'
+  autoload :NoSelectorForElement,  'site_prism/exceptions'
+  autoload :TimeOutWaitingForElementVisibility,  'site_prism/exceptions'
+  autoload :TimeOutWaitingForElementInvisibility,  'site_prism/exceptions'
+
+  class AccessDeniedError < RuntimeError ; end
+  class AssertAccessDeniedError < AccessDeniedError ; end
+end
