0052ccc49e233d2ced051e4b6719ed0b736bf9d8
Paul Yakshankin <yakspavel@undev.ru>
2013-09-30 09:08:29 +0000

    fixed exceptions autoload

--- lib/site_prism.rb
+++ lib/site_prism.rb
@@ -1,17 +1,9 @@
+require 'site_prism/exceptions'
+
 module SitePrism
   autoload :Addressable,  'addressable/template'
-  autoload :Version,  'site_prism/version'
   autoload :ElementContainer,  'site_prism/element_container'
   autoload :ElementChecker,  'site_prism/element_checker'
   autoload :Page,  'site_prism/page'
   autoload :Section,  'site_prism/section'
-
-  autoload :NoUrlForPage,  'site_prism/exceptions'
-  autoload :NoUrlMatcherForPage,  'site_prism/exceptions'
-  autoload :NoSelectorForElement,  'site_prism/exceptions'
-  autoload :TimeOutWaitingForElementVisibility,  'site_prism/exceptions'
-  autoload :TimeOutWaitingForElementInvisibility,  'site_prism/exceptions'
-
-  class AccessDeniedError < RuntimeError ; end
-  class AssertAccessDeniedError < AccessDeniedError ; end
 end
