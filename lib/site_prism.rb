86030a2d3bc1141b749c7d208a292f671df9fd2a
Nat Ritmeyer <nat@natontesting.com>
2013-09-30 14:04:22 +0100

    not autoloading addressable - cannot get it to work!?

--- lib/site_prism.rb
+++ lib/site_prism.rb
@@ -1,9 +1,10 @@
 require 'site_prism/exceptions'
+require 'addressable/template'
 
 module SitePrism
-  autoload :Addressable,  'addressable/template'
   autoload :ElementContainer,  'site_prism/element_container'
   autoload :ElementChecker,  'site_prism/element_checker'
   autoload :Page,  'site_prism/page'
   autoload :Section,  'site_prism/section'
 end
+
