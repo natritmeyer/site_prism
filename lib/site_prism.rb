3ab60ac18ec157c897b24f3a5612d6a62addfc08
Nat Ritmeyer <nat@natontesting.com>
2013-10-02 12:30:46 +0100

    autoloading waiter class

--- lib/site_prism.rb
+++ lib/site_prism.rb
@@ -6,9 +6,10 @@ module SitePrism
   autoload :ElementChecker,  'site_prism/element_checker'
   autoload :Page,  'site_prism/page'
   autoload :Section,  'site_prism/section'
+  autoload :Waiter, 'site_prism/waiter'
 end
 
-require 'site_prism/waiter'
+# require 'site_prism/waiter'
 require 'site_prism/version'
 require 'site_prism/config'
 
