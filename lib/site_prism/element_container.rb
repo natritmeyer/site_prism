839e6a461f5c4349755094734529833e2e3c3f3d
Constantine Zaytsev <bassneck@gmail.com>
2014-01-25 12:04:34 +1300

    Pass the section class block directly to Class.new

--- lib/site_prism/element_container.rb
+++ lib/site_prism/element_container.rb
@@ -170,8 +170,7 @@ module SitePrism::ElementContainer
     if args.first.is_a? Class
       section_class = args.shift
     elsif block_given?
-      section_class = Class.new SitePrism::Section
-      section_class.class_eval &block
+      section_class = Class.new SitePrism::Section, &block
     else
       raise ArgumentError.new "You should provide section class either as a block, or as the second argument"
     end
