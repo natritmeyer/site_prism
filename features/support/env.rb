25d40bee1d5e7dfedcf2621224266168bbfda5b8
RustyNail <takonoyawarakaage@yahoo.co.jp>
2018-03-04 18:15:52 +0900

    Add chrome and its driver settings.
    Make profile according to browser.
    
    Define private method.

--- features/support/env.rb
+++ features/support/env.rb
@@ -44,10 +44,23 @@ Capybara.configure do |config|
 end
 
 Capybara.register_driver :selenium do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new
+  profile = if chrome?
+              Selenium::WebDriver::Chrome::Profile.new
+            else
+              Selenium::WebDriver::Firefox::Profile.new
+            end
   profile['browser.cache.disk.enable'] = false
   profile['browser.cache.memory.enable'] = false
-  Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile)
+  Capybara::Selenium::Driver.new(app, browser: browser, profile: profile)
+
+  private
+  def chrome?
+    browser == :chrome
+  end
+
+  def browser
+    ENV['TARGET_BROWSER'].to_sym
+  end
 end
 
 SitePrism.configure do |config|
