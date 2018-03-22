7fee0db84af00211959741a3d6e204b6f5e5acf7
Luke Hill <luke.hill@onthebeach.co.uk>
2018-03-22 08:54:32 +0000

    Internal Review: Spec Refactor

--- spec/section_spec.rb
+++ spec/section_spec.rb
@@ -2,45 +2,41 @@
 
 require 'spec_helper'
 
+class Section < SitePrism::Section; end
+class Page < SitePrism::Page; end
+
 describe SitePrism::Page do
   describe '.section' do
-    class Page < SitePrism::Page; end
-    class Section < SitePrism::Section; end
-
-    class PageWithAnonymousSection < SitePrism::Page
-      section :anonymous_section, '.section' do |s|
-        s.element :title, 'h1'
-      end
-    end
-
-    class PageWithSection < SitePrism::Page
-      section :section, Section, '.section'
-    end
-
     context 'second argument is a Class' do
-      subject { PageWithSection.new }
-
-      it 'should create a section using the Class' do
-        expect(subject).to respond_to(:section)
+      class PageWithSection < SitePrism::Page
+        section :section, Section, '.section'
       end
+
+      subject(:page_with_section) { PageWithSection.new }
+
+      it { is_expected.to respond_to(:section) }
     end
 
     context 'second argument is not a Class and a block given' do
-      subject { PageWithAnonymousSection.new }
-
-      it 'should create an anonymous section using the block' do
-        expect(subject).to respond_to(:anonymous_section)
+      class PageWithAnonymousSection < SitePrism::Page
+        section :anonymous_section, '.section' do |s|
+          s.element :title, 'h1'
+        end
       end
+
+      subject(:page_with_anonymous_section) { PageWithAnonymousSection.new }
+
+      it { is_expected.to respond_to(:anonymous_section) }
     end
 
     context 'second argument is not a Class and no block given' do
-      subject { Page.section(:incorrect_section, '.section') }
+      subject(:invalid_page) { Page.section(:incorrect_section, '.section') }
       let(:error_message) do
         'You should provide section class either as a block, or as the second argument.'
       end
 
-      it 'should raise an ArgumentError' do
-        expect { subject }
+      it 'raises an ArgumentError' do
+        expect { invalid_page }
           .to raise_error(ArgumentError)
           .with_message(error_message)
       end
@@ -49,85 +45,98 @@ describe SitePrism::Page do
 end
 
 describe SitePrism::Section do
-  class Page < SitePrism::Page; end
-
-  subject { SitePrism::Section.new(Page.new, locator) }
-  let(:locator) { object_double(Capybara::Node::Element.new(:foo, :bar, :baz, :ignore)) }
+  let(:instance) { SitePrism::Section.new(Page.new, locator) }
+  let(:locator) { instance_double('Capybara::Node::Element') }
   let(:section_with_block) { SitePrism::Section.new(Page.new, locator) { 1 + 1 } }
 
-  it 'responds to element' do
-    expect(SitePrism::Section).to respond_to(:element)
+  describe 'Object' do
+    subject { SitePrism::Section }
+
+    it { is_expected.to respond_to(:element) }
+    it { is_expected.to respond_to(:elements) }
   end
 
-  it 'responds to elements' do
-    expect(SitePrism::Section).to respond_to(:elements)
+  describe '#new' do
+    context 'with a block' do
+      it 'passes the block to Capybara.within' do
+        expect(Capybara).to receive(:within).with(locator)
+
+        section_with_block
+      end
+    end
+
+    context 'without a block' do
+      it 'does not pass a block to Capybara.within' do
+        expect(Capybara).not_to receive(:within)
+
+        instance
+      end
+    end
   end
 
-  describe 'instance' do
-    it 'passes a given block to Capybara.within' do
-      expect(Capybara).to receive(:within).with(locator)
-
-      section_with_block
-    end
-
-    it 'does not require a block' do
-      expect(Capybara).not_to receive(:within)
-
-      subject
-    end
-
-    it 'evaluates visibility by delegating through root_element' do
+  describe '#visible?' do
+    it 'delegates through root_element' do
       expect(locator).to receive(:visible?)
 
-      subject.visible?
+      instance.visible?
     end
+  end
 
-    it 'obtains the text of a section by delegating through root_element' do
+  describe '#text' do
+    it 'delegates through root_element' do
       expect(locator).to receive(:text)
 
-      subject.text
+      instance.text
     end
+  end
 
-    it 'obtains the native property of a section by delegating through root_element' do
+  describe '#native' do
+    it 'delegates through root_element' do
       expect(locator).to receive(:native)
 
-      subject.native
+      instance.native
     end
+  end
 
-    it 'executes scripts using Capybara' do
+  describe '#execute_script' do
+    it 'delegates through Capybara' do
       expect(Capybara.current_session).to receive(:execute_script).with('JUMP!')
 
-      subject.execute_script('JUMP!')
+      instance.execute_script('JUMP!')
     end
+  end
 
-    it 'evaluates scripts using Capybara' do
+  describe '#evaluate_script' do
+    it 'delegates through Capybara' do
       expect(Capybara.current_session).to receive(:evaluate_script).with('How High?')
 
-      subject.evaluate_script('How High?')
+      instance.evaluate_script('How High?')
     end
+  end
 
-    describe '#parent_page' do
-      let(:section) { SitePrism::Section.new(page, '.locator') }
-      let(:deeply_nested_section) do
+  describe '#parent_page' do
+    let(:section) { SitePrism::Section.new(page, '.locator') }
+    let(:deeply_nested_section) do
+      SitePrism::Section.new(
         SitePrism::Section.new(
           SitePrism::Section.new(
-            SitePrism::Section.new(
-              page, '.locator-section-large'
-            ), '.locator-section-medium'
-          ), '.locator-small'
-        )
-      end
-      let(:page) { Page.new }
+            page, '.locator-section-large'
+          ), '.locator-section-medium'
+        ), '.locator-small'
+      )
+    end
+    let(:page) { Page.new }
 
-      it 'returns the parent of a section' do
-        expect(section.parent_page).to eq(page)
+    it 'returns the parent of a section' do
+      expect(section.parent_page.class).to eq(Page)
 
-        expect(section.parent_page).to be_a SitePrism::Page
-      end
+      expect(section.parent_page).to be_a SitePrism::Page
+    end
 
-      it 'returns the parent page of a deeply nested section' do
-        expect(deeply_nested_section.parent_page).to be_a SitePrism::Page
-      end
+    it 'returns the parent page of a deeply nested section' do
+      expect(deeply_nested_section.parent_page.class).to eq(Page)
+
+      expect(deeply_nested_section.parent_page).to be_a SitePrism::Page
     end
   end
 end
