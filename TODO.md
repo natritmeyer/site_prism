601c837071fd65353a691d5fcbaaa19123928501
Luke Hill <luke.hill@onthebeach.co.uk>
2018-07-09 13:42:11 +0100

    Final dump of files

--- TODO.md
+++ TODO.md
@@ -4,7 +4,21 @@
 -  Update of Ruby Version to supported version
 -  Rubocop LineLength reduction (Continue this, will be hard work, probably several PR's)
 -  Create iFrame specs (Even though private methods)
-- Generic spec walkthrough - (have done section/sections/element/elements/top half of page)
 - Advanced spec clean. Make sure each spec file represents the right items
 - Setup a secondary gemfile to track against older items (As and when we upgrade Ruby/Gem deps)
 - Standardise wait key assignment in element container (Work broken out from Capybara3 rework)
+- Create Test Helper and isolate all classes into single definition/s (CSS/XPath pages)
+
+Generic Spec walkthrough
+
+addressable - Done
+configure - Done
+element - Done
+elements - Done
+iframe - Done
+loadable - Done
+page - 40% complete
+section - TODO
+sections - TODO
+version - TODO
+waiter - TODO
--- spec/elements_spec.rb
+++ spec/elements_spec.rb
@@ -3,14 +3,40 @@
 require 'spec_helper'
 
 describe SitePrism::Page do
-  subject { Page.new }
-
-  class Page < SitePrism::Page
-    elements :bobs, 'a.b c.d'
-  end
-
-  context 'with elements `bobs` defined' do
+  shared_examples 'elements' do
     it { is_expected.to respond_to(:bobs) }
     it { is_expected.to respond_to(:has_bobs?) }
+    it { is_expected.to respond_to(:has_no_bobs?) }
+    it { is_expected.to respond_to(:wait_for_bobs) }
+    it { is_expected.to respond_to(:wait_for_no_bobs) }
+    it { is_expected.to respond_to(:all_there?) }
+
+    it 'should return an enumerable result' do
+      expect(subject.bobs).to be_a Capybara::Result
+    end
+  end
+
+  context 'with css elements' do
+    class PageCSS < SitePrism::Page
+      elements :bobs, 'a.b c.d'
+    end
+
+    subject { page }
+    let(:page) { PageCSS.new }
+    let(:klass) { PageCSS }
+
+    it_behaves_like 'elements'
+  end
+
+  context 'with xpath elements' do
+    class PageXPath < SitePrism::Page
+      elements :bobs, '//a[@class="b"]//c[@class="d"]'
+    end
+
+    subject { page }
+    let(:page) { PageXPath.new }
+    let(:klass) { PageXPath }
+
+    it_behaves_like 'elements'
   end
 end
