57e94f012027fd1e9910a621a775f2d529f29b8b
Luke Hill <lukehill_uk@hotmail.com>
2018-10-02 09:16:57 +0100

    Ensure tests are correct (Couple weren't asserting their gherkin definitions

--- features/step_definitions/section_explicit_waiting_steps.rb
+++ features/step_definitions/section_explicit_waiting_steps.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 When('I wait for the section that takes a while to appear') do
-  @test_site.section_experiments.slow_section(wait: 1)
+  @test_site.home.slow_section(wait: 1)
 end
 
 When('I wait for the section that takes a while to vanish') do
-  @test_site.section_experiments.wait_until_removing_parent_invisible
+  @test_site.home.wait_until_vanishing_section_invisible
 end
 
 Then("an exception is raised when I wait for a section that won't appear") do
@@ -14,27 +14,23 @@ Then("an exception is raised when I wait for a section that won't appear") do
 end
 
 Then('an error is raised when waiting for the section to vanish') do
-  page = @test_site.section_experiments
-
-  expect { page.wait_until_anonymous_section_invisible }
+  expect { @test_site.home.wait_until_container_invisible }
     .to raise_error(SitePrism::ElementInvisibilityTimeoutError)
 end
 
-Then('the removed section is not present') do
-  expect(@test_site.section_experiments).not_to have_removing_parent
+Then('the section is no longer visible') do
+  expect(@test_site.home.vanishing_section).not_to be_visible
 end
 
 When('I wait an overridden time for the section to vanish') do
-  @test_site.section_experiments.wait_until_removing_parent_invisible(wait: 2)
+  @test_site.home.wait_until_vanishing_section_invisible(wait: 2)
 end
 
 Then('the slow section appears') do
-  expect(@test_site.section_experiments).to have_slow_section
+  expect(@test_site.home).to have_slow_section
 end
 
 Then('an error is raised when waiting a new time for the section to vanish') do
-  page = @test_site.section_experiments
-
-  expect { page.wait_until_anonymous_section_invisible(wait: 0.15) }
+  expect { @test_site.home.wait_until_container_invisible(wait: 0.15) }
     .to raise_error(SitePrism::ElementInvisibilityTimeoutError)
 end
