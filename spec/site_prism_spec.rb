b0f93e6757cbee1b0cced56cc49b429c14011828
Luke Hill <lukehill_uk@hotmail.com>
2019-01-29 09:51:52 +0100

    Alter tests that failed to now pass due to new config settings. Add gitignore in preparation for file based log tests

--- spec/site_prism_spec.rb
+++ spec/site_prism_spec.rb
@@ -3,49 +3,39 @@
 require 'spec_helper'
 
 describe SitePrism do
-  describe '.enable_logging' do
-    it 'is disabled by default' do
-      expect(SitePrism.enable_logging).to be false
-    end
-
-    it 'can be configured to log information' do
-      SitePrism.configure { |config| config.enable_logging = true }
-
-      expect(SitePrism.enable_logging).to be true
-    end
-  end
-
   describe '.logger' do
     # This stops the stdout process leaking between tests
     after(:each) { wipe_logger! }
 
-    it 'does not log messages below UNKNOWN' do
-      log_messages = capture_stdout do
-        SitePrism.logger.debug('DEBUG')
-        SitePrism.logger.info('INFO')
-        SitePrism.logger.warn('WARN')
-        SitePrism.logger.error('ERROR')
-        SitePrism.logger.fatal('FATAL')
+    context 'at default severity' do
+      it 'does not log messages below UNKNOWN' do
+        log_messages = capture_stdout do
+          SitePrism.logger.debug('DEBUG')
+          SitePrism.logger.info('INFO')
+          SitePrism.logger.warn('WARN')
+          SitePrism.logger.error('ERROR')
+          SitePrism.logger.fatal('FATAL')
+        end
+
+        expect(log_messages).to be_empty
       end
 
-      expect(log_messages).to be_empty
+      it 'logs UNKNOWN level messages' do
+        log_messages = capture_stdout do
+          SitePrism.log_level = "debug"
+          SitePrism.logger.info('INFO')
+          SitePrism.logger.unknown('UNKNOWN')
+        end
+
+        expect(log_messages).not_to be_empty
+      end
     end
 
-    it 'logs UNKNOWN level messages' do
-      log_messages = capture_stdout do
-        SitePrism.logger.unknown('UNKNOWN')
-      end
-
-      expect(log_messages).not_to be_empty
-    end
-
-    context 'with logging enabled' do
-      before do
-        SitePrism.configure { |config| config.enable_logging = true }
-      end
-
+    context 'at an altered severity' do
       it 'logs messages at all levels' do
         log_messages = capture_stdout do
+          set_logger_to_debug
+
           SitePrism.logger.debug('DEBUG')
           SitePrism.logger.info('INFO')
           SitePrism.logger.warn('WARN')
@@ -72,6 +62,10 @@ describe SitePrism do
     return unless SitePrism.instance_variable_get(:@logger)
 
     SitePrism.remove_instance_variable(:@logger)
+  end
+
+  def set_logger_to_debug
+    SitePrism.log_level = "debug"
   end
 
   def lines(string)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov' unless ENV['CI']
-
 require 'capybara'
 require 'capybara/dsl'
 
@@ -15,12 +14,6 @@ Capybara.default_max_wait_time = 0
 
 RSpec.configure do |rspec|
   rspec.default_formatter = :documentation
-
-  rspec.after(:each) do
-    SitePrism.configure do |config|
-      config.enable_logging = false
-    end
-  end
 end
 
 class MyTestApp
