b6bda83c9b93d982ec5dca26fdfa2a15637d06ab
Luke Hill <lukehill_uk@hotmail.com>
2019-03-13 09:29:23 +0100

    Initial work. Completed unit tests for code coverage for 0/1/invalid recursion

--- spec/site_prism/element_spec.rb
+++ spec/site_prism/element_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 describe 'Element' do
+  # This stops the stdout process leaking between tests
+  before(:each) { wipe_logger! }
+
   shared_examples 'an element' do
     describe '.element' do
       it 'should be settable' do
@@ -19,14 +22,60 @@ describe 'Element' do
     describe '#all_there?' do
       subject { page.all_there? }
 
-      context 'with no recursion' do
-        it { is_expected.to be_truthy }
+      context 'by default' do
+        it { is_expected.to be true }
 
         it 'checks only the expected elements' do
           expect(page).to receive(:there?).with(:element_one).once
           expect(page).not_to receive(:there?).with(:element_two)
 
           subject
+        end
+      end
+
+      context 'with recursion set to none' do
+        subject { page.all_there?(recursion: 'none') }
+
+        it { is_expected.to be true }
+
+        it 'checks only the expected elements' do
+          expect(page).to receive(:there?).with(:element_one).once
+          expect(page).not_to receive(:there?).with(:element_two)
+
+          subject
+        end
+      end
+
+      context 'with recursion set to one' do
+        subject { page.all_there?(recursion: 'one') }
+
+        it { is_expected.to be true }
+
+        it 'checks only the expected elements' do
+          expect(page).to receive(:there?).with(:element_one).once
+          expect(page).not_to receive(:there?).with(:element_two)
+
+          subject
+        end
+      end
+
+      context 'with recursion set to an invalid value' do
+        subject { page.all_there?(recursion: 'go nuts') }
+
+        it 'does not check any elements' do
+          expect(page).not_to receive(:there?)
+
+          subject
+        end
+
+        it 'sends an error to the SitePrism logger' do
+          SitePrism.configure { |config| config.enable_logging = true }
+
+          log_messages = capture_stdout do
+            subject
+          end
+
+          expect(lines(log_messages)).to eq(2)
         end
       end
     end
