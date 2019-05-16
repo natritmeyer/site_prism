abc103f8b7b2e879524c23e72a89794f2d66802d
Luke Hill <lukehill_uk@hotmail.com>
2019-05-16 15:14:55 +0200

    Configure the sections correctly/fix load order

--- spec/fixtures/css_section.rb
+++ spec/fixtures/css_section.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CSSSection < SitePrism::Page
+class CSSSection < SitePrism::Section
   element :inner_element_one, '.one'
   element :inner_element_two, '.two'
   iframe :iframe, CSSIFrame, '.iframe'
--- spec/fixtures/xpath_section.rb
+++ spec/fixtures/xpath_section.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class XPathSection < SitePrism::Page
+class XPathSection < SitePrism::Section
   element :inner_element_one, :xpath, '//*[@class="one"]'
   element :inner_element_two, :xpath, '//*[@class="two"]'
   iframe :iframe, XPathIFrame, :xpath, '//*[@class="iframe"]'
