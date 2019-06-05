99e22ff4743f6da84fde20b28e9bd463d3848e92
Luke Hill <lukehill_uk@hotmail.com>
2019-06-05 10:28:35 +0200

    Remove the need for dotenv as we've not needed the customisation initially thought
    Update capybara to 3.3+ to include some selenium standardisation and some generic fixes
    
    Use an undefined driver name  just to mitigate vs any hacker wanting to use the default selenium driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-unless ENV['CI']
-  require 'simplecov'
-  require 'dotenv'
-  Dotenv.load('.env')
-end
-
+require 'simplecov' unless ENV['CI']
 require 'capybara'
-require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'webdrivers'
@@ -20,13 +14,13 @@ require 'site_prism'
 require_relative 'js_helper'
 require_relative 'sections/all'
 
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :site_prism do |app|
   browser = ENV.fetch('browser', 'firefox').to_sym
   Capybara::Selenium::Driver.new(app, browser: browser)
 end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium
+  config.default_driver = :site_prism
   config.default_max_wait_time = 0.75
   config.app_host = 'file://' + File.dirname(__FILE__) + '/../../test_site'
   config.ignore_hidden_elements = false
